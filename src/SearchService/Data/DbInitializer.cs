3be26cffdb16f41f903da0603c0795a2fcac9cf1
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-10-25 19:07:34 +0700

    010 Adding Http communication to get the data

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -20,16 +21,14 @@ public class DbInitializer
 
         var count = await DB.CountAsync<Item>();
 
-        if(count == 0)
-        {
-            Console.WriteLine("No data - will attempt to seed");
-            var itemData = await File.ReadAllTextAsync("Data/auctions.json");
+        using var scope = app.Services.CreateScope();
 
-            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+        var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+        var items = await httpClient.GetItemsForSearchDb();
+        Console.WriteLine(items.Count + " returned from the auction service--------------------------------------------");
 
-            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+        if( items.Count > 0 ) await DB.SaveAsync(items);
+        
 
-            await DB.SaveAsync(items);
-        }
     }
 }
