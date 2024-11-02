2106d329f36bd1224a7ef709e567397d704d5d6c
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-11-02 09:06:19 +0700

    008 Adding the remaining consumers in the Search Service

--- src/Contracts/BidPlaced.cs
+++ src/Contracts/BidPlaced.cs
@@ -9,5 +9,5 @@ public class BidPlaced
     public string Bidder { get; set; }
     public DateTime BidTime { get; set; }
     public string BidStatus { get; set; }
-    public int? Amount { get; set; }
+    public int Amount { get; set; }
 }
