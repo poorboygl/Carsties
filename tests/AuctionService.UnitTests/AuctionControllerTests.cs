15b019963b0cc6c6370f1eb36aefcf2f4eceaa27
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-11-04 12:10:11 +0700

    007 Unit testing the GET methods part 1

--- tests/AuctionService.UnitTests/AuctionControllerTests.cs
+++ tests/AuctionService.UnitTests/AuctionControllerTests.cs
@@ -1,10 +1,12 @@
 using System;
 using AuctionService.Controllers;
 using AuctionService.Data;
+using AuctionService.DTOs;
 using AuctionService.RequestHelpers;
 using AutoFixture;
 using AutoMapper;
-using MassTransit.Transports;
+using MassTransit;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace AuctionService.UnitTests;
@@ -12,7 +14,7 @@ namespace AuctionService.UnitTests;
 public class AuctionControllerTests
 {
     private readonly Mock<IAuctionRepository> _auctionRepo;
-    private readonly Mock<PublishEndpoint> _publishEndpoint;
+    private readonly Mock<IPublishEndpoint> _publishEndpoint;
     private readonly Fixture _fixture;
     private readonly AuctionsController _controller;
     private readonly IMapper _mapper;
@@ -21,12 +23,27 @@ public class AuctionControllerTests
     {
         _fixture = new Fixture();
         _auctionRepo = new Mock<IAuctionRepository>();
-        _publishEndpoint =  new Mock<PublishEndpoint>();
+        _publishEndpoint =  new Mock<IPublishEndpoint>();
         var mockMapper = new MapperConfiguration(mc => {
                 mc.AddMaps(typeof(MappingProfiles).Assembly);
         }).CreateMapper().ConfigurationProvider;
         _mapper = new Mapper(mockMapper);
         _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+    }
+
+    [Fact]
+    public async Task GetAuctions_WithNoParams_Return10Auctions()
+    {
+        //arrange
+        var auctions =  _fixture.CreateMany<AuctionDto>(10).ToList();
+        _auctionRepo.Setup(repo => repo.GetAuctionsAsync(null)).ReturnsAsync(auctions);
+
+        //act
+        var result = await _controller.GetAllAuctions(null);
+
+        //assert
+        Assert.Equal(10,result.Value.Count());
+        Assert.IsType<ActionResult<List<AuctionDto>>>(result);
     }
 
 }
