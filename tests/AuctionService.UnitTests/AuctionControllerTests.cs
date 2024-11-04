67ce89078451000fc6c987c52b045256dee71efe
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-11-04 11:40:18 +0700

    006 Mocking things

---
+++ tests/AuctionService.UnitTests/AuctionControllerTests.cs
@@ -0,0 +1,32 @@
+using System;
+using AuctionService.Controllers;
+using AuctionService.Data;
+using AuctionService.RequestHelpers;
+using AutoFixture;
+using AutoMapper;
+using MassTransit.Transports;
+using Moq;
+
+namespace AuctionService.UnitTests;
+
+public class AuctionControllerTests
+{
+    private readonly Mock<IAuctionRepository> _auctionRepo;
+    private readonly Mock<PublishEndpoint> _publishEndpoint;
+    private readonly Fixture _fixture;
+    private readonly AuctionsController _controller;
+    private readonly IMapper _mapper;
+
+    public AuctionControllerTests()
+    {
+        _fixture = new Fixture();
+        _auctionRepo = new Mock<IAuctionRepository>();
+        _publishEndpoint =  new Mock<PublishEndpoint>();
+        var mockMapper = new MapperConfiguration(mc => {
+                mc.AddMaps(typeof(MappingProfiles).Assembly);
+        }).CreateMapper().ConfigurationProvider;
+        _mapper = new Mapper(mockMapper);
+        _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+    }
+
+}
