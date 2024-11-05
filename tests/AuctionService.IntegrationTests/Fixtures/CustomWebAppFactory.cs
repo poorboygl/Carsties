ff8950716aa6dd998f1ecb9b80d3c1c2d1dd7acb
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-11-05 19:15:43 +0700

    013 Setting up a Custom web application factory for integration testing

--- tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -1,8 +1,45 @@
+using AuctionService.Data;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Testcontainers.PostgreSql;
 
 namespace AuctionService.IntegrationTests.Fixtures;
 
-public class CustomWebAppFactory : WebApplicationFactory<Program>
+public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
 {
+    private PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+    public async Task InitializeAsync()
+    {
+        await _postgreSqlContainer.StartAsync();
+    }
 
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services => {
+            var descriptor = services.SingleOrDefault(d => 
+                d.ServiceType == typeof(DbContextOptions<AuctionDbContext>));
+
+            if(descriptor != null) services.Remove(descriptor);
+
+            services.AddDbContext<AuctionDbContext>(option => {
+                option.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+            });
+
+            services.AddMassTransitTestHarness();
+            var sp = services.BuildServiceProvider();
+            using var scope = sp.CreateScope();
+            var scopeServices = scope.ServiceProvider;
+            var db = scopeServices.GetRequiredService<AuctionDbContext>();
+
+            db.Database.Migrate();
+
+        });
+    }
+
+    Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
 }
