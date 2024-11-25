135e807c5b8f3263168edbf7f4d49df341dda077
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-11-25 23:34:21 +0700

    005 Fetching data from the API

--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Listings from "./auctions/Listings";
 
 export default function Home() {
   return (
     <div>
-      content goes here !
+      <Listings />
     </div>
   );
 }
