5eaf1bc80a55ea14fc424808a3a555ed4937da18
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-11-27 00:09:17 +0700

    019 Adding the sorting functionality

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,14 +1,53 @@
 import { useParamsStore } from '@/hooks/useParamsStore';
 import { Button, ButtonGroup } from 'flowbite-react';
 import React from 'react'
+import { AiOutlineClockCircle, AiOutlineSortAscending, AiOutlineSortDescending } from 'react-icons/ai';
+import { BsFillStopCircleFill } from 'react-icons/bs';
 
 const pageSizeButtons = [4, 8, 12];
+
+const orderButtons = [
+    {
+        label: 'Alphabetical',
+        icon: AiOutlineSortAscending,
+        value: 'make'
+    },
+    {
+        label: 'End date',
+        icon: AiOutlineClockCircle,
+        value: 'endingsoon'
+    },
+    {
+        label: 'Recently added',
+        icon: BsFillStopCircleFill,
+        value: 'new'
+    },
+]
 
 export default function Filters() {
     const pageSize = useParamsStore(state => state.pageSize);
     const setParams = useParamsStore(state => state.setParams);
+    const orderBy = useParamsStore(state => state.orderBy);
+
     return (
         <div className='flex justify-between items-center mb-4'>
+
+            <div>
+                <span className='uppercase text-sm text-gray-500 mr-2'>Order By</span>
+                <ButtonGroup>
+                    {orderButtons.map(({label, icon: Icon, value}) => (
+                        <Button
+                            key = {value}
+                            onClick={() => setParams({orderBy: value})} 
+                            color={`${orderBy === value ? 'red' : 'gray' }`}
+                        >
+                            <Icon className= 'mr-3 h-4 w-4'/>
+                            {label}
+                        </Button>
+                    ))}
+                </ButtonGroup>
+            </div>
+
             <div>
                 <span className='uppercase text-sm text-gray-500 mr-2'> page size</span>
                 <ButtonGroup>
