c5cfb540bab83e1bc0c871d354e9dd8c7e6a9cdb
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-11-28 22:15:57 +0700

    007 Populating the User actions dropdown

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -1,11 +1,46 @@
-import { Button } from 'flowbite-react'
-import Link from 'next/link'
-import React from 'react'
+'use client'
 
-export default function UserActions() {
+import { Dropdown, DropdownDivider, DropdownItem } from 'flowbite-react'
+import { User } from 'next-auth'
+import { signOut } from 'next-auth/react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
+import { HiCog, HiUser } from 'react-icons/hi'
+
+type Props = {
+    user: User
+}
+
+export default function UserActions({user} : Props) {
+  const router = useRouter();
   return (
-    <Button outline>
-        <Link href='/session'>Session</Link>
-    </Button>
+    <Dropdown inline label = {`Welcome ${user.name}`}>
+      <DropdownItem icon = {HiUser}>
+        <Link href='/'>
+          My Auctions
+        </Link>
+      </DropdownItem>
+      <DropdownItem icon = {AiFillTrophy}>
+        <Link href='/'>
+          Auction won
+        </Link>
+      </DropdownItem>
+      <DropdownItem icon = {AiFillCar}>
+        <Link href='/'>
+          Sell My Car
+        </Link>
+      </DropdownItem>
+      <DropdownItem icon = {HiCog}>
+        <Link href='/session'>
+          Session (dev only!)
+        </Link>
+      </DropdownItem>
+      <DropdownDivider />
+      <DropdownItem icon = {AiOutlineLogout} onClick={() =>  signOut({callbackUrl: '/'} )}>
+        Sign out
+      </DropdownItem>
+    </Dropdown>
   )
 }
