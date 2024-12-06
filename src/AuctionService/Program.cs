da73f1f47402323a40f496e10959f9e45a0c5ebe
Nguyen Vu <nguyenquocvu.16494@gmail.com>
2024-12-06 14:40:32 +0700

    013 Adding gRPC part 1

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,5 +1,7 @@
+using AuctionService;
 using AuctionService.Consumers;
 using AuctionService.Data;
+using AuctionService.Services;
 using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -42,6 +44,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username";
     });
 builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
+builder.Services.AddGrpc();
 
 
 var app = builder.Build();
@@ -52,6 +55,7 @@ app.UseAuthorization();
 
 
 app.MapControllers();
+app.MapGrpcService<GrpcAuctionService>();
 
 try
 {
